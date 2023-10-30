83cf49178eb244877c761deccf21d11ee531d2ef
Slotheve <85428225+Slotheve@users.noreply.github.com>
2023-10-30 20:34:08 +0800

    Update hysteria.sh

--- hysteria.sh
+++ hysteria.sh
@@ -333,7 +333,7 @@ checkSystem
 menu() {
 	clear
 	echo "################################"
-	echo -e "#      ${RED}Hysteria一键安装脚本${PLAIN}       #"
+	echo -e "#      ${RED}Hysteria一键安装脚本${PLAIN}    #"
 	echo -e "# ${GREEN}作者${PLAIN}: 怠惰(Slotheve)         #"
 	echo -e "# ${GREEN}网址${PLAIN}: https://slotheve.com   #"
 	echo -e "# ${GREEN}频道${PLAIN}: https://t.me/SlothNews #"
